69040bfb654c0144c0b7ebc657bfe342ecbc4085
DESKTOP-OHMCF31\PC-20180528W <applyk99@163.com>
2018-11-06 16:50:23 +0800

    modify

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,5 @@
 const devConf = require('../webpack.config')
 
-const pathResolve = (part) => {
-  return path.resolve(__dirname, '../', part)
-}
-
 module.exports = {
   ...devConf,
   mode: 'production',
